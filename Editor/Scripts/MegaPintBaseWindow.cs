2bea0c303667e6f29ffc778a4bf216eebc3d1c31
Tiogiras <tiogiras@gmail.com>
2023-08-05 18:22:24 +0200

    Base Window

---
+++ Editor/Scripts/MegaPintBaseWindow.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor.Scripts
+{
+    public class MegaPintBaseWindow : MegaPintEditorWindowBase
+    {
+        private const string BasePath = "User Interface/MegaPintBaseWindow";
+
+        private static MegaPintBaseWindow _instance;
+        private static VisualTreeAsset _baseWindow;
+
+        [MenuItem("MegaPint/Open")]
+        private static void ShowWindow()
+        {
+            if (!LoadResourceContent())
+                return;
+
+            _instance ??= CreateInstance<MegaPintBaseWindow>();
+            _instance.titleContent.text = "MegaPint";
+            _instance.Show();
+        }
+
+        private void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
+        }
+
+        private static bool LoadResourceContent()
+        {
+            Debug.Log("loading...");
+            _baseWindow = Resources.Load<VisualTreeAsset>(BasePath);
+            return _baseWindow != null;
+        }
+
+        private void CreateGUI()
+        {
+            var root = rootVisualElement;
+
+            VisualElement content = _baseWindow.Instantiate();
+
+            root.Add(content);
+        }
+    }
+}
---
+++ Editor/Scripts/MegaPintEditorWindowBase.cs
@@ -0,0 +1,9 @@
+using UnityEditor;
+
+namespace Editor
+{
+    public abstract class MegaPintEditorWindowBase : EditorWindow
+    {
+        
+    }
+}
