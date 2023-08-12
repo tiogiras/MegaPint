64a055acccf24ee3df510f4861c8fb0cb543a5b7
Tiogiras <tiogiras@gmail.com>
2023-08-12 16:10:28 +0200

    Reverted Test

--- Editor/Scripts/PackageManager/MegaPintPackagesData.cs
+++ Editor/Scripts/PackageManager/MegaPintPackagesData.cs
@@ -35,7 +35,7 @@ namespace Editor.Scripts.PackageManager
                 PackageName = "com.tiogiras.megapint-autosave",
                 PackageNiceName = "Scene-AutoSave",
                 GitUrl = "https://github.com/tiogiras/MegaPint-AutoSave.git",
-                Version = "1.0.2",
+                Version = "1.0.0",
                 LastUpdate = "12.08.2023",
                 UnityVersion = "2021.3.20f1 or higher",
                 MegaPintVersion = "1.0.0 or higher"
