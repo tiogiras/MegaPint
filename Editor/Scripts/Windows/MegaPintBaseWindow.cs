5dd4b8249ea071cc296ac2992a2607f43a806d07
Tiogiras <tiogiras@gmail.com>
2023-08-12 16:43:11 +0200

    Test

--- Editor/Scripts/Windows/MegaPintBaseWindow.cs
+++ Editor/Scripts/Windows/MegaPintBaseWindow.cs
@@ -1,4 +1,8 @@
 ﻿#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Linq;
+using Editor.Scripts.PackageManager;
+using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -6,8 +10,27 @@ namespace Editor.Scripts.Windows
 {
     public class MegaPintBaseWindow : MegaPintEditorWindowBase
     {
+        private const string PackageItem = "User Interface/MegaPintBasePackageItem";
+        private const string SettingItem = "User Interface/MegaPintBaseSettingItem";
+        
         /// <summary> Loaded reference of the uxml </summary>
         private VisualTreeAsset _baseWindow;
+        private VisualTreeAsset _packageItem;
+        private VisualTreeAsset _settingItem;
+
+        private Button _btnPackages;
+        private Button _btnSettings;
+
+        private ListView _packagesList;
+        private ListView _settingsList;
+
+        private ToolbarSearchField _searchField;
+        
+        private Label _loading;
+
+        private MegaPintPackageManager.CachedPackages _allPackages;
+        private List<MegaPintPackagesData.MegaPintPackageData> _displayedPackages;
+        private MegaPintPackagesData.MegaPintPackageData _selectedPackage;
 
         #region Overrides
 
@@ -29,18 +52,101 @@ namespace Editor.Scripts.Windows
 
             content.Q<Button>("OpenImporter").clicked += OpenImporter;
 
+            _btnPackages = content.Q<Button>("BTN_Packages");
+            _btnSettings = content.Q<Button>("BTN_Settings");
+
+            _btnPackages.clicked += UpdatePackages;
+            _btnSettings.clicked += UpdateSettings;
+
+            _packagesList = content.Q<ListView>("PackagesList");
+            _settingsList = content.Q<ListView>("SettingsList");
+            
+            _packagesList.makeItem = () => _packageItem.Instantiate();
+            _settingsList.makeItem = () => _settingItem.Instantiate();
+
+            _packagesList.bindItem = (element, i) =>
+            {
+                element.Q<Label>("PackageName").text = _displayedPackages[i].PackageNiceName;
+            };
+            
+            _settingsList.bindItem = (element, i) => { };
+
+            _packagesList.destroyItem = element => element.Clear();
+            _settingsList.destroyItem = element => element.Clear();
+            
+            _loading = content.Q<Label>("Loading");
+            
+            _loading.style.display = DisplayStyle.Flex;
+            _packagesList.style.display = DisplayStyle.None;
+            _settingsList.style.display = DisplayStyle.None;
+
+            _searchField = content.Q<ToolbarSearchField>("SearchField");
+            _searchField.RegisterValueChangedCallback(SearchFieldChange);
+            
+            UpdatePackages();
+            
             root.Add(content);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            _searchField.UnregisterValueChangedCallback(SearchFieldChange);
+            
+            _btnPackages.clicked -= UpdatePackages;
+            _btnSettings.clicked -= UpdateSettings;
         }
 
         protected override bool LoadResources()
         {
             _baseWindow = Resources.Load<VisualTreeAsset>(BasePath());
-            return _baseWindow != null;
+            _packageItem = Resources.Load<VisualTreeAsset>(PackageItem);
+            _settingItem = Resources.Load<VisualTreeAsset>(SettingItem);
+
+            return _baseWindow != null && _packageItem != null && _settingItem != null;
         }
 
         #endregion
         
         public static void OpenImporter() => ContextMenu.TryOpen<MegaPintPackageManagerWindow>(true);
+
+        private void SearchFieldChange(ChangeEvent<string> evt) => SetDisplayedPackages(_searchField.value);
+        
+        public void UpdatePackages()
+        {
+            _allPackages = new MegaPintPackageManager.CachedPackages(_loading, () =>
+            {
+                SetDisplayedPackages(_searchField.value);
+            });
+        }
+        
+        private void SetDisplayedPackages(string searchString)
+        {
+            _loading.style.display = DisplayStyle.None;
+            SwitchState(0);
+            
+            _displayedPackages = searchString.Equals("") ? 
+                _allPackages.ToDisplay().Where(package => _allPackages.IsImported(package.PackageKey)).ToList() :
+                _allPackages.ToDisplay().Where(package => _allPackages.IsImported(package.PackageKey))
+                    .Where(package => package.PackageNiceName.ToLower().Contains(searchString.ToLower())).ToList();
+            
+            _displayedPackages.Sort();
+
+            _packagesList.itemsSource = _displayedPackages;
+            _packagesList.RefreshItems();
+        }
+
+        public void UpdateSettings()
+        {
+            
+        }
+
+        private void SwitchState(int page)
+        {
+            _packagesList.style.display = page == 0 ? DisplayStyle.Flex : DisplayStyle.None;
+            _packagesList.style.display = page == 1 ? DisplayStyle.Flex : DisplayStyle.None;
+        }
     }
 }
 #endif
