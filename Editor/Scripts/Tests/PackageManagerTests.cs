75f5258f79816c37107d2d7d6b3afec7879e12d2
Tiogiras <tiogiras@gmail.com>
2024-02-23 17:00:59 +0100

    Testing

--- Editor/Scripts/Tests/PackageManagerTests.cs
+++ Editor/Scripts/Tests/PackageManagerTests.cs
@@ -12,12 +12,13 @@ namespace Editor.Scripts.Tests
 
 public class PackageManagerTests
 {
+    private bool _initialized;
     private bool _result;
     private bool _waiting;
-    
-    private bool _initialized;
-    
-    [UnityTest, Order(0)]
+
+    #region Tests
+
+    [UnityTest] [Order(0)]
     public IEnumerator CacheInitialized()
     {
         _initialized = false;
@@ -25,23 +26,18 @@ public class PackageManagerTests
 
         PackageCache.onCacheRefreshed += CacheRefreshed;
         PackageCache.Refresh();
-        
+
         while (!_initialized && currentTime < 200)
         {
             currentTime += Time.deltaTime;
+
             yield return null;
         }
 
         Assert.IsTrue(_initialized);
     }
-
-    private void CacheRefreshed()
-    {
-        PackageCache.onCacheRefreshed -= CacheRefreshed;
-        _initialized = true;
-    }
     
-    [UnityTest, Order(1)]
+    [UnityTest] [Order(1)]
     public IEnumerator ImportPackage()
     {
         MegaPintPackageManager.onSuccess += Success;
@@ -49,26 +45,18 @@ public class PackageManagerTests
 
         _result = false;
         _waiting = true;
-        
+
 #pragma warning disable CS4014
         MegaPintPackageManager.AddEmbedded(PackageCache.Get(PackageKey.AlphaButton));
 #pragma warning restore CS4014
 
         while (_waiting)
-        {
             yield return null;
-        }
-        
+
         Assert.IsTrue(_result);
     }
     
-    [Test, Order(2)]
-    public void DependenciesRegistered()
-    {
-        Assert.IsFalse(PackageCache.Get(PackageKey.Validators).CanBeRemoved(out List <Dependency> _));
-    }
-    
-    [UnityTest, Order(2)]
+    [UnityTest] [Order(2)]
     public IEnumerator ImportPackageVariation()
     {
         MegaPintPackageManager.onSuccess += Success;
@@ -76,20 +64,24 @@ public class PackageManagerTests
 
         _result = false;
         _waiting = true;
-        
+
 #pragma warning disable CS4014
         MegaPintPackageManager.AddEmbedded(PackageCache.Get(PackageKey.AlphaButton).Variations[0]);
 #pragma warning restore CS4014
 
         while (_waiting)
-        {
             yield return null;
-        }
-        
+
         Assert.IsTrue(_result);
     }
-    
-    [UnityTest, Order(2)]
+
+    [Test] [Order(3)]
+    public void DependenciesRegistered()
+    {
+        Assert.IsFalse(PackageCache.Get(PackageKey.Validators).CanBeRemoved(out List <Dependency> _));
+    }
+
+    [UnityTest] [Order(4)]
     public IEnumerator RemovePackage()
     {
         MegaPintPackageManager.onSuccess += Success;
@@ -97,20 +89,18 @@ public class PackageManagerTests
 
         _result = false;
         _waiting = true;
-        
+
 #pragma warning disable CS4014
         MegaPintPackageManager.Remove(PackageCache.Get(PackageKey.AlphaButton).Name);
 #pragma warning restore CS4014
 
         while (_waiting)
-        {
             yield return null;
-        }
-        
+
         Assert.IsTrue(_result);
     }
     
-    [UnityTest, Order(2)]
+    [UnityTest] [Order(5)]
     public IEnumerator RemoveFormerDependencyPackage()
     {
         MegaPintPackageManager.onSuccess += Success;
@@ -118,28 +108,25 @@ public class PackageManagerTests
 
         _result = false;
         _waiting = true;
-        
+
 #pragma warning disable CS4014
         MegaPintPackageManager.Remove(PackageCache.Get(PackageKey.Validators).Name);
 #pragma warning restore CS4014
 
         while (_waiting)
-        {
             yield return null;
-        }
-        
+
         Assert.IsTrue(_result);
     }
-    
-    
 
-    private void Success()
+    #endregion
+
+    #region Private Methods
+
+    private void CacheRefreshed()
     {
-        MegaPintPackageManager.onSuccess -= Success;
-        MegaPintPackageManager.onFailure -= Failure;
-
-        _waiting = false;
-        _result = true;
+        PackageCache.onCacheRefreshed -= CacheRefreshed;
+        _initialized = true;
     }
 
     private void Failure(string error)
@@ -151,6 +138,17 @@ public class PackageManagerTests
         _waiting = false;
         _result = false;
     }
+
+    private void Success()
+    {
+        MegaPintPackageManager.onSuccess -= Success;
+        MegaPintPackageManager.onFailure -= Failure;
+
+        _waiting = false;
+        _result = true;
+    }
+
+    #endregion
 }
 
 }
