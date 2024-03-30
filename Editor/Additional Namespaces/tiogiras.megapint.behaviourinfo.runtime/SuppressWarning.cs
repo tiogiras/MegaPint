8c526bb5a979f0ec3df99dc357f1a8d65307b0ac
Tiogiras <tiogiras@gmail.com>
2024-03-30 14:40:43 +0100

    Package Initialization

---
+++ Editor/Additional Namespaces/tiogiras.megapint.behaviourinfo.runtime/SuppressWarning.cs
@@ -0,0 +1,10 @@
+namespace Editor.Additional_Namespaces.tiogiras.megapint.notepad.runtime
+{
+
+// Class is used to suppress assembly not used warning when the package is not installed
+internal class SuppressWarning
+{
+
+}
+
+}
---
+++ Editor/Scripts/PackageManager/Packages/Data/PackageDataBehaviourInfo.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+namespace Editor.Scripts.PackageManager.Packages.Data
+{
+
+/// <summary> Data for the NotePad package </summary>
+internal static class PackageDataBehaviourInfo
+{
+    #region Public Methods
+
+    /// <summary> Get the data for this package </summary>
+    /// <returns> Corresponding <see cref="PackageData" /> </returns>
+    public static PackageData Get()
+    {
+        return new PackageData
+        {
+            key = PackageKey.BehaviourInfo,
+            reqMpVersion = "1.3.0 or higher",
+            version = "1.0.0",
+            unityVersion = "2022.3.15f1",
+            lastUpdate = "",
+            name = "com.tiogiras.megapint-behaviourinfo",
+            displayName = "Behaviour Info",
+            description = "",
+            repository = "https://github.com/tiogiras/MegaPint-BehaviourInfo.git"
+        };
+    }
+
+    #endregion
+}
+
+}
+#endif
