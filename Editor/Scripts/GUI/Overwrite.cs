48a5a0686cee08a3970d0c38aee8721d0bd0536c
Tiogiras <tiogiras@gmail.com>
2024-04-15 12:11:24 +0200

    Testing

--- Editor/Scripts/GUI/Overwrite.cs
+++ Editor/Scripts/GUI/Overwrite.cs
@@ -6,6 +6,7 @@ public enum Overwrite
 {
     mpPrimaryColorText, mpPrimaryColorBorder, mpTextColor, mpTextColorSecondary, mpBackground1,
     mpBackground2, mpBackground3, mpInteraction, mpBg1AsBorderColor, mpPrimaryAsImageTint,
+    
     mpTextSecondaryAsImageTint, mpInteractionImageOnly, mpTextRed, mpTextGreen, mp_border_separator
 }
 
