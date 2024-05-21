1024b95681769e36b1a15a326fb9bf04d251c30a
Tiogiras <tiogiras@gmail.com>
2024-05-21 16:45:47 +0200

    GUI

---
+++ Editor/Scripts/GUI/Factories/Backgrounds/BackgroundQuaternary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UIElements;
+
+namespace Editor.Scripts.GUI.Factories.Backgrounds
+{
+
+public class BackgroundQuaternary : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory <BackgroundQuaternary, UxmlTraits> { }
+
+    public new class UxmlTraits : VisualElement.UxmlTraits
+    {
+        #region Public Methods
+
+        public override void Init(
+            VisualElement element,
+            IUxmlAttributes attributes,
+            CreationContext context)
+        {
+            base.Init(element, attributes, context);
+            
+            element.AddToClassList(StyleSheetClasses.Background.Color.Quaternary);
+        }
+
+        #endregion
+    }
+}
+
+}
