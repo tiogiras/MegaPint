e484d7cccc7450545defa15672decc5f95a8a7d1
Tiogiras <tiogiras@gmail.com>
2024-05-24 21:46:02 +0200

    Applied Fixes

--- Editor/Scripts/ContextMenu.cs
+++ Editor/Scripts/ContextMenu.cs
@@ -1,10 +1,13 @@
 ﻿#if UNITY_EDITOR
+using System.IO;
+using System.Linq;
 using Editor.Scripts.PackageManager.Cache;
 using Editor.Scripts.Settings;
 using Editor.Scripts.Windows;
 using Editor.Scripts.Windows.DevMode;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.WSA;
 
 namespace Editor.Scripts
 {
@@ -36,7 +39,35 @@ internal static partial class ContextMenu
     [MenuItem(MenuItemMegaPint + "/Test", false, 0)]
     private static void Test()
     {
-        PackageCache.Refresh();
+        //PackageCache.Refresh();
+        
+        foreach (Object o in Selection.objects)
+        {
+            var path = AssetDatabase.GetAssetPath(o);
+
+            if (!Directory.Exists(path))
+                continue;
+
+            Debug.Log($"Searching in {path}");
+            
+            var files = Directory.GetFiles(path, "*.uxml", SearchOption.AllDirectories);
+
+            Debug.Log($"Found{files.Length}");
+            
+            foreach (var file in files)
+            {
+                var text = File.ReadAllText(file);
+
+                if (text.Contains("MegaPint.Editor.Scripts.GUI.Factories"))
+                    continue;
+                
+                text = text.Replace("Editor.Scripts.GUI.Factories", "MegaPint.Editor.Scripts.GUI.Factories");
+                
+                File.WriteAllText(file, text);
+            }
+            
+            AssetDatabase.Refresh();
+        }
     }
 
     [MenuItem(MenuItemMegaPint + "/Open", false, 0)]
