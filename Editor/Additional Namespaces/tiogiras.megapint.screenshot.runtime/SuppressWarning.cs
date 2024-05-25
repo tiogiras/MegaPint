f93bd73abd7c3daf9d472f85a2e80b90eb3fea3d
Tiogiras <tiogiras@gmail.com>
2024-05-25 15:07:09 +0200

    SupressWarning scripts namepsace changes

--- Editor/Additional Namespaces/tiogiras.megapint.screenshot.runtime/SuppressWarning.cs
+++ Editor/Additional Namespaces/tiogiras.megapint.screenshot.runtime/SuppressWarning.cs
@@ -1,10 +1,9 @@
-namespace Editor.Additional_Namespaces.tiogiras.megapint.screenshot.runtime
+namespace MegaPint.Editor.Additional_Namespaces.tiogiras.megapint.screenshot.runtime
 {
 
 // Class is used to suppress assembly not used warning when the package is not installed
 internal class SuppressWarning
 {
-
 }
 
 }
--- Editor/Additional Namespaces/tiogiras.megapint.validators.runtime/SuppressWarning.cs
+++ Editor/Additional Namespaces/tiogiras.megapint.validators.runtime/SuppressWarning.cs
@@ -1,10 +1,9 @@
-namespace Editor.Additional_Namespaces.tiogiras.megapint_validators.Runtime.Scripts
+namespace MegaPint.Editor.Additional_Namespaces.tiogiras.megapint.validators.runtime
 {
 
 // Class is used to suppress assembly not used warning when the package is not installed
 internal class SuppressWarning
 {
-
 }
 
 }
