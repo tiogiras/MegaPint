d54d1a64ef57b934d019f8d02008e4adabd943bf
Tiogiras <tiogiras@gmail.com>
2024-06-11 18:35:35 +0200

    PackageManager Unit Tests

--- Editor/Scripts/Tests/PackageImport/Screenshot.cs
+++ Editor/Scripts/Tests/PackageImport/Screenshot.cs
@@ -1,12 +1,66 @@
 ﻿#if UNITY_EDITOR
 #if UNITY_INCLUDE_TESTS
+using System.Collections;
+using System.Threading.Tasks;
+using MegaPint.Editor.Scripts.PackageManager.Packages;
+using MegaPint.Editor.Scripts.Tests.Utility;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
 namespace MegaPint.Editor.Scripts.Tests.PackageImport
 {
 
 /// <summary> Unit tests testing the import of the Screenshot package </summary>
 internal class Screenshot
 {
-    // TODO
+    private static bool s_initialized;
+
+    #region Tests
+
+    [UnityTest] [Order(1)]
+    public IEnumerator ImportPackage()
+    {
+        TestsUtility.SkipIfProductionProject();
+
+        if (!s_initialized)
+            Assert.Fail("FAILED ===> Missing packageCache initialization!");
+
+        Task <bool> task = TestsUtility.ImportPackage(PackageKey.Screenshot);
+
+        yield return task.AsIEnumeratorReturnNull();
+
+        Assert.IsTrue(task.Result);
+    }
+
+    [UnityTest] [Order(0)]
+    public IEnumerator InitializePackageCache()
+    {
+        TestsUtility.SkipIfProductionProject();
+
+        Task <bool> task = TestsUtility.CheckCacheInitialization();
+
+        yield return task.AsIEnumeratorReturnNull();
+
+        s_initialized = task.Result;
+        Assert.IsTrue(task.Result);
+    }
+
+    [UnityTest] [Order(4)]
+    public IEnumerator RemovePackage()
+    {
+        TestsUtility.SkipIfProductionProject();
+
+        if (!s_initialized)
+            Assert.Fail("FAILED ===> Missing packageCache initialization!");
+
+        Task <bool> task = TestsUtility.RemovePackage(PackageKey.Screenshot);
+
+        yield return task.AsIEnumeratorReturnNull();
+
+        Assert.IsTrue(task.Result);
+    }
+
+    #endregion
 }
 
 }
