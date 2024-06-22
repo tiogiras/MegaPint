56d00722ae2b28079d45660b3ca436efbaeff02d
Tiogiras <tiogiras@gmail.com>
2024-06-22 14:56:35 +0200

    Updated Packages Data

--- Editor/Scripts/PackageManager/Packages/Data/PackageDataPlayModeStartScene.cs
+++ Editor/Scripts/PackageManager/Packages/Data/PackageDataPlayModeStartScene.cs
@@ -14,10 +14,10 @@ internal static class PackageDataPlayModeStartScene
         return new PackageData
         {
             key = PackageKey.PlayModeStartScene,
-            reqMpVersion = "1.1.1 or higher",
-            version = "1.0.1",
+            reqMpVersion = "1.2.2 or higher",
+            version = "1.0.2",
             unityVersion = "2022.3.15f1",
-            lastUpdate = "22.02.2024",
+            lastUpdate = "22.06.2024",
             name = "com.tiogiras.megapint-playmodestartscene",
             displayName = "PlayMode Start Scene",
             description =
