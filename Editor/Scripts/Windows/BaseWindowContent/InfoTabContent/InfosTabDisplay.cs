720deeddb69fbf8f89bfa56151f4c6ba771f19f5
tiogiras <tiogiras@gmail.com>
2025-08-21 22:09:40 +0200

    Implemented Suggestions

--- Editor/Scripts/Windows/BaseWindowContent/InfoTabContent/InfosTabDisplay.cs
+++ Editor/Scripts/Windows/BaseWindowContent/InfoTabContent/InfosTabDisplay.cs
@@ -1,8 +1,12 @@
 ﻿#if UNITY_EDITOR
 using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using MegaPint.Editor.Scripts.GUI.Utility;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UIElements;
 using GUIUtility = MegaPint.Editor.Scripts.GUI.Utility.GUIUtility;
 
@@ -12,6 +16,14 @@ namespace MegaPint.Editor.Scripts.Windows.BaseWindowContent.InfoTabContent
 /// <summary> Contains logic for displaying the info tabs </summary>
 internal static class InfosTabDisplay
 {
+    [System.Serializable]
+    public class ReportData
+    {
+        public string title;
+        public string message;
+        public string type;
+    }
+    
     private static string s_basePath;
 
     private static string _BasePath =>
@@ -43,6 +55,11 @@ internal static class InfosTabDisplay
                 ContactLogic(root);
 
                 break;
+            
+            case InfosTabData.InfoKey.Report:
+                ReportLogic(root);
+
+                break;
 
             case InfosTabData.InfoKey.ManagePackages:
                 ManagePackagesLogic(root);
@@ -68,6 +85,171 @@ internal static class InfosTabDisplay
         }
     }
 
+    /// <summary> Logic of the report info </summary>
+    /// <param name="root"> Info as <see cref="VisualElement" /> </param>
+    private static async Task ReportLogic(VisualElement root)
+    {
+        VisualElement loadingPanel = root.Q ("Loading");
+        VisualElement contentPanel = root.Q ("Content");
+        VisualElement errorPanel = root.Q ("Error");
+        
+        loadingPanel.style.display = DisplayStyle.Flex;
+        contentPanel.style.display = DisplayStyle.None;
+        errorPanel.style.display = DisplayStyle.None;
+        
+        errorPanel.ActivateLinks(evt =>
+        {
+            switch (evt.linkID)
+            {
+                case "github":
+                    Application.OpenURL("https://github.com/tiogiras/MegaPint");
+
+                    break;
+            }
+        });
+        
+        UnityWebRequest request = UnityWebRequest.Get("https://tiogiras.games/megapint_api/checkup_report.php");
+        
+        UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+        while (!operation.isDone)
+        {
+            await Task.Yield();
+        }
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            loadingPanel.style.display = DisplayStyle.None;
+            errorPanel.style.display = DisplayStyle.Flex;
+            
+            return;
+        }
+        
+        request = UnityWebRequest.Get("https://tiogiras.games/megapint_api/get_versions.php");
+        
+        operation = request.SendWebRequest();
+        
+        while (!operation.isDone)
+        {
+            await Task.Yield();
+        }
+        
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            loadingPanel.style.display = DisplayStyle.None;
+            errorPanel.style.display = DisplayStyle.Flex;
+            
+            return;
+        }
+        
+        loadingPanel.style.display = DisplayStyle.None;
+        contentPanel.style.display = DisplayStyle.Flex;
+        
+        ReportSuggestionLogic(contentPanel.Q("Suggestion"));
+        ReportIssueLogic(contentPanel.Q("Issue"));
+    }
+
+    /// <summary> Holds the logic for setting up the make a suggestion tab </summary>
+    /// <param name="root"> Root element of the tab </param>
+    private static void ReportSuggestionLogic(VisualElement root)
+    {
+        var noTitle = root.Q <Label>("NoTitle");
+        var noMessage = root.Q <Label>("NoMessage");
+
+        noTitle.style.display = DisplayStyle.None;
+        noMessage.style.display = DisplayStyle.None;
+        
+        VisualElement success = root.Q("Success");
+        VisualElement fail = root.Q("Fail");
+        
+        success.style.display = DisplayStyle.None;
+        fail.style.display = DisplayStyle.None;
+        
+        var btnSend = root.Q <Button>("BTN_Send");
+
+        btnSend.clickable = new Clickable(() =>
+        {
+            var title = root.Q <TextField>("Title");
+            var message = root.Q <TextField>("Message");
+            
+            var hasTitle = !string.IsNullOrEmpty(title.text);
+            var hasMessage = !string.IsNullOrEmpty(message.text);
+
+            noTitle.style.display = hasTitle ? DisplayStyle.None : DisplayStyle.Flex;
+            noMessage.style.display = hasMessage ? DisplayStyle.None : DisplayStyle.Flex;
+            
+            if (!hasTitle || !hasMessage)
+                return;
+
+            _ = PostReport(btnSend, title, message, success, fail);
+        });
+    }
+
+    private static async Task PostReport(Button button, TextField title, TextField message, VisualElement success, VisualElement error)
+    {
+        var json = JsonUtility.ToJson(new ReportData
+        {
+            title = title.text,
+            message = message.text,
+            type = "Suggestion"
+        });
+
+        var request = new UnityWebRequest("https://tiogiras.games/megapint_api/post_report.php", "POST");
+        request.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(json));
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        button.SetEnabled(false);
+
+        UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+        while (!operation.isDone)
+        {
+            await Task.Yield();
+        }
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            await FadeResult(error, button);
+            return;
+        }
+
+        title.value = string.Empty;
+        message.value = string.Empty;
+        
+        await FadeResult(success, button);
+    }
+    
+    private static async Task FadeResult(VisualElement element, Button button)
+    {
+        element.style.display = DisplayStyle.Flex;
+        element.style.opacity = 1.0f;
+
+        const float fadeDuration = 1.0f;
+        const int steps = 30;
+        const float stepTime = fadeDuration / steps;
+
+        for (var i = 0; i < steps; i++)
+        {
+            var t = (i + 1) / (float)steps;
+            element.style.opacity = 1.0f - t;
+
+            await Task.Delay((int)(stepTime * 1000));
+        }
+
+        element.style.display = DisplayStyle.None;
+        
+        button.SetEnabled(true);
+    }
+
+    
+    /// <summary> Holds the logic for setting up the report an issue tab </summary>
+    /// <param name="root"> Root element of the tab </param>
+    private static void ReportIssueLogic(VisualElement root)
+    {
+        //TODO
+    }
+    
     /// <summary> Logic of the contact info </summary>
     /// <param name="root"> Info as <see cref="VisualElement" /> </param>
     private static void ContactLogic(VisualElement root)
